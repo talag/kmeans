7686714b37efdf977c9406991bcc7334bf00af83
talag <agmontal@gmail.com>
2017-07-08 22:01:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -97,3 +97,11 @@ class Labels (webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/clustering/labels', Labels),
 ], debug=True)
+
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host='0.0.0.0', port='8080')
+
+if __name__ == '__main__':
+    main()
+
