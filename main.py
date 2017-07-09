c24d6c5102dd65271187ef51234f13c7fae17721
talag <agmontal@gmail.com>
2017-07-09 16:11:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-# coding: utf-7
-
 import numpy as np
 import random
 import math
 import urllib
+import webapp2
+import logging
 
 # Kmeans algorithm:
 # fill 'cluster' column in the given dataset with k clusters
-# input: 
+# input:
 #   points - a numpy matrix containing all points
-#   k - number of clusters 
+#   k - number of clusters
 #   max_iter - maximum number of iterations
 
 
 class KMeans(object):
-    def __init__(self, k=4, max_iter=300):
+    def __init__(self, k, max_iter):
         self.k = k
         self.max_iter = max_iter
 
@@ -22,86 +22,109 @@ class KMeans(object):
     def _random_centroids(self, points):
         centroids = points[np.random.choice(points.shape[0], self.k, replace=False), :]
         return centroids
-    
+
     # returns an array with indexes to the nearest centroid for each point in points
     def _nearest_neigbours(self, points, centroids):
-        # add new dimension to centroids, use broadcasting to substract all centroids from each point, 
+        # add new dimension to centroids, use broadcasting to substract all centroids from each point,
         # such that each sub space of credentials contains results for one point vs. centroids
         distances = np.sqrt(((points - centroids[:, np.newaxis]) ** 2).sum(axis=2))
         return np.argmin(distances, axis=0)
 
-    # returns updated centroids according to the means of the assigned points    
+    # returns updated centroids according to the means of the assigned points
     def _update_centroids(self, points, centroids, labels):
         return np.array([points[labels == k].mean(axis=0) for k in range(centroids.shape[0])])
-    
+
     def kmeans(self, points):
-	points = np.array(points)
-        # first step: initialization: generate k random clusters 
+        points = np.array(points)
+        # first step: initialization: generate k random clusters
         centroids = self._random_centroids(points)
-    
+
         # label datapoints with current centroids
         labels = self._nearest_neigbours(points, centroids)
-    
+
         # this turns to false when clusters remained the same after some iteration, which means we should stop
         clusters_updated = True
-    
-        # initialize cluster columon which will later contain the label 
-        i = 0
-    
-        while (i < self.max_iter and clusters_updated):
-            # Save centroids for testing convergence.
-            old_centroids = centroids
-        
-            # Calculate new centroids based on datapoint current labels
-            centroids = self._update_centroids(points, centroids, labels)
 
-            # label datapoints with new centroids
-            labels = self._nearest_neigbours(points, centroids)
-    
-            # convergence test
-            if (np.array_equal(centroids, old_centroids)):
-                clusters_updated = False
-        
-            i += 1
-    
+        # initialize cluster columon which will later contain the label
+        i = 0
+
+        while (i < self.max_iter and clusters_updated):
+                # Save centroids for testing convergence.
+                old_centroids = centroids
+
+                # Calculate new centroids based on datapoint current labels
+                centroids = self._update_centroids(points, centroids, labels)
+
+                # label datapoints with new centroids
+                labels = self._nearest_neigbours(points, centroids)
+
+                # convergence test
+                if (np.array_equal(centroids, old_centroids)):
+                        clusters_updated = False
+
+                i = 1
+
         # the output labels should start from index 1
-        labels += 1
-        points = np.c_[points,labels] 
+        labels = np.add(labels, 1)
+        points = np.c_[points,labels]
         return points
 
-import webapp2
-
 class Labels (webapp2.RequestHandler):
-	def _parse_input_data(self, points):
-		points = urllib.unquote_plus(points).replace(' ','')
-		#remove '[]' brackets
-		points = points[1:-2]
-		points_list = points.split("=&\\n")
-    		new_list = []
-    		for i in range(len(points_list)):
-        		# remove trailing ';'
-        		if points_list[i][-1]==';':
-            			points_list[i] = points_list[i][:-1]
-        		new_list.append(tuple(float(x) for x in points_list[i].split(',')))
-		return new_list
+        DEFAULT_K = 4
+        DEFAULT_MAX_ITER = 300
 
-	def post(self):
-                k = self.request.get('num_clusters', default_value=4)
-                max_iter = self.request.get('max_iterations', default_value=300)
-                points = self._parse_input_data(self.request.body)
+        def _parse_input_data(self, points):
+                #remove '[]' brackets and convert to list
+                points_list = points[1:-1].split(";")
+                new_list = []
+                for i in range(len(points_list)):
+                        new_list.append(list(float(x) for x in points_list[i].split(',')))
+                return new_list
+        def _points_str_for_output(self, points):
+                output_str = '['
+                for i in points:
+                        #convert each inner array to string and add ';'
+                        output_str += np.array2string(i, separator = ', ', formatter = {'float_kind':lambda x: "%.1f" % x})[1:-1] + ';'
+                #remove last ';' and add closing ']'
+                output_str = output_str[:-1] + ']'
+                return output_str
+        def post(self):
+                try:
+                        # initialize with request paramenters
+                        k = self.request.get('num_clusters', default_value = self.DEFAULT_K)
+                        max_iter = self.request.get('max_iterations', default_value = self.DEFAULT_MAX_ITER)
+                        points = self._parse_input_data(self.request.body)
+                except Exception as err:
+                        self._handle_400(self.request, self.response, err)
+
+                # prepare the response
                 self.response.headers['Content-Type'] = 'text/plain'
-                labeled_points = KMeans(k, max_iter).kmeans(points)
-		self.response.write('k is {k}, max_iter is {max_iter}, points are {points}\n'.format(k=k, max_iter=max_iter, points=labeled_points))
+                try:
+                        labeled_points = KMeans(k, max_iter).kmeans(points)
+                        labeled_points = self._points_str_for_output(labeled_points)
+                except Exception as err:
+                        self._handle_500(self.request, self.response, err)
 
+                self.response.write('{points}\n'.format(k=k, max_iter=max_iter, points=labeled_points))
+
+        def _handle_400(self, request, response, exception):
+                logging.exception(exception)
+                response.write('Bad Request!')
+                self.abort(400)
+
+        def _handle_500(self, request, response, exception):
+                logging.exception(exception)
+                response.write('A server error occurred!')
+                self.abort(500)
 
 app = webapp2.WSGIApplication([
     ('/clustering/labels', Labels),
 ], debug=True)
 
+
 def main():
     from paste import httpserver
-    httpserver.serve(app, host='0.0.0.0', port='8080')
+    httpserver.serve(app, host='0.0.0.0', port='80')
 
 if __name__ == '__main__':
     main()
-
